6edbd6fc946495475329039ae8f1b4d873365dc5
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-03-03 18:31:42 -0300

    feature/tags

---
+++ Ploomes.Api.Client/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace Ploomes.Api.Client.Models {
+    /// <remarks/>
+    public sealed class Tag {
+        /// <remarks/>
+        public int Id { get; set; }
+        /// <remarks/>
+        public string Name { get; set; }
+        /// <remarks/>
+        public string Color { get; set; }
+        /// <remarks/>
+        public int EntityId { get; set; }
+    }
+}
